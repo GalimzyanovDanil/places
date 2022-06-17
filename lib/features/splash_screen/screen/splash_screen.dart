a9a9f180c70f32a08c0ec9fe889917d4eb480cd1
Danil_Galimzyanov <galimzyanov.dev@icloud.com>
2022-06-17 16:07:12 +0300

    chore(T-23): Сплэш скрин анимация

--- lib/features/splash_screen/screen/splash_screen.dart
+++ lib/features/splash_screen/screen/splash_screen.dart
@@ -1,5 +1,8 @@
 import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:places/assets/colors/app_colors.dart';
+import 'package:places/assets/res/app_assets.dart';
 import 'package:places/features/splash_screen/screen/splash_wm.dart';
 
 // TODO(me): cover with documentation
@@ -12,9 +15,23 @@ class SplashScreen extends ElementaryWidget<ISplashWidgetModel> {
 
   @override
   Widget build(ISplashWidgetModel wm) {
-    return const Center(
-      child:
-          SizedBox(height: 50, width: 50, child: CircularProgressIndicator()),
+    return DecoratedBox(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [AppColors.yellowSplash, AppColors.greenSplash],
+        ),
+      ),
+      child: Center(
+        child: RotationTransition(
+          turns: wm.animation,
+          child: SvgPicture.asset(
+            AppAssets.iconSplashLogo,
+            color: AppColors.whiteBase,
+            width: 160,
+            height: 160,
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/features/splash_screen/screen/splash_wm.dart
+++ lib/features/splash_screen/screen/splash_wm.dart
@@ -8,7 +8,9 @@ import 'package:places/features/splash_screen/screen/splash_model.dart';
 import 'package:places/features/splash_screen/screen/splash_screen.dart';
 import 'package:provider/provider.dart';
 
-abstract class ISplashWidgetModel extends IWidgetModel {}
+abstract class ISplashWidgetModel extends IWidgetModel {
+  Animation<double> get animation;
+}
 
 SplashWidgetModel defaultSplashWidgetModelFactory(BuildContext context) {
   final appScope = context.read<IAppScope>();
@@ -19,9 +21,16 @@ SplashWidgetModel defaultSplashWidgetModelFactory(BuildContext context) {
 // TODO(me): cover with documentation
 /// Default widget model for SplashWidget
 class SplashWidgetModel extends WidgetModel<SplashScreen, SplashModel>
+    with TickerProviderWidgetModelMixin
     implements ISplashWidgetModel {
   final Coordinator coordinator;
+
   late final bool isOnboardingFinish;
+  late final Animation<double> _animation;
+  late final AnimationController _controller;
+
+  @override
+  Animation<double> get animation => _animation;
 
   SplashWidgetModel({required SplashModel model, required this.coordinator})
       : super(model);
@@ -29,16 +38,41 @@ class SplashWidgetModel extends WidgetModel<SplashScreen, SplashModel>
   @override
   void initWidgetModel() {
     super.initWidgetModel();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    _animation = Tween<double>(
+      begin: 0,
+      end: -1,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _controller.repeat();
+
     _initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   Future<void> _initialize() async {
     isOnboardingFinish = await model.getOnboardingStatus();
-
-    _navigate();
+    await Future<void>.delayed(const Duration(seconds: 3))
+        .whenComplete(() async {
+      _navigate();
+    });
   }
 
-  //
   void _navigate() {
     if (isMounted) {
       if (isOnboardingFinish) {
