0ae6e57fb8d750277e4a57749684f98a3f0cf730
Danil_Galimzyanov <galimzyanov.dev@icloud.com>
2022-07-08 11:11:59 +0300

    chore(T-33): Новые интеграционные тесты экрана фильтр

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,3 +1,24 @@
+import 'dart:io';
+
 import 'package:integration_test/integration_test_driver.dart';
 
-Future<void> main() => integrationDriver();
+Future<void> main() async {
+  const adbPath = '/Users/danil/Developer/Android/platform-tools/adb';
+
+  await Process.run(adbPath, [
+    'shell',
+    'pm',
+    'grant',
+    'ru.surfstudio.flutterTemplate',
+    'android.permission.ACCESS_FINE_LOCATION',
+  ]);
+  await Process.run(adbPath, [
+    'shell',
+    'pm',
+    'grant',
+    'ru.surfstudio.flutterTemplate',
+    'android.permission.ACCESS_COARSE_LOCATION',
+  ]);
+
+  await integrationDriver();
+}
